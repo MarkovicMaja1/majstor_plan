955b83ec8122c6f24cfa0677c2bd1b5c953272d7
MarkovicMaja1 <majamarkovic.b@gmail.com>
2024-05-31 14:42:57 +0200

    changes

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -2,6 +2,7 @@ import React from "react";
 import PickMeals from "../Assets/ceramics.png";
 import ChooseMeals from "../Assets/floor.png";
 import DeliveryMeals from "../Assets/gips.jpg";
+import miniBager from '../Assets/mbg.webp'
 
 const Work = () => {
   const workInfoData = [
@@ -20,14 +21,14 @@ const Work = () => {
       title: "Gipsarski radovi",
       text: "Naš tim majstora pruža širok spektar usluga gipsarskih radova, uključujući gipsane zidove, plafone, dekorativne elemente i još mnogo toga. Sa pažnjom prema detaljima i stručnošću, garantujemo visokokvalitetne rezultate koji će zadovoljiti sve vaše potrebe."
     },
-    {
+    {image: miniBager,
       // Dodajemo novu karticu za Iskop mini bagerom, behaton, uredjenje dvorista
       title: "Iskop mini bagerom, behaton, uredjenje dvorista",
       text: "Osiguravamo profesionalne usluge iskopa mini bagerom, postavljanja behatona i uređenja dvorišta. Bez obzira na vaše potrebe, možete računati na nas za kvalitetnu izradu i uredjenje vašeg prostora."
     },
     {
       // Dodajemo novu karticu za Postavljanje sigurnosnih kamera i automatizacija prostora
-      title: "Postavljanje sigurnosnih kamera i automatizacija prostora",
+      title: "Postavljanje sigurnosnih kamera i automatizacija prostora (ACDC Novi Sad)",
       text: "Nudimo usluge postavljanja sigurnosnih kamera i automatizacije prostora za vašu bezbednost i udobnost. Naš tim stručnjaka će vam pomoći da osigurate svoj prostor i olakšate svakodnevne aktivnosti uz najnovije tehnološke inovacije."
     },
   ];
